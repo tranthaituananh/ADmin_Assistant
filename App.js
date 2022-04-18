0e09e8f293887b3f4a86f864d3c9d9599d27d7dd
tranthaituananh <19521219@gm.uit.edu.vn>
2022-04-18 22:06:47 +0700

    login (fix_1)

--- App.js
+++ App.js
@@ -1,70 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from "react"
+import { StyleSheet, View, Text, Image, ImageBackground } from "react-native"
+import Login from "./src/screens/Login"
 
-import React from 'react';
-import type {Node} from 'react';
-import Login from './src/screens/Login';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <Login />
-    </SafeAreaView>
-  );
-};
-
-export default App;
+export default function App() {
+  return <Login />
+}
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { StyleSheet, Image, Text, View, ImageBackground } from "react-native";
+import React from "react"
+import { StyleSheet, Image, Text, View, ImageBackground } from "react-native"
 
 export default function SignIn() {
   return (
     <View style={styles.SignIn}>
-      <View style={styles.Group852}>
-        <Text style={styles.Txt815}>Welcome Back!</Text>
+      <View style={styles.Group1053}>
+        <Text style={styles.Txt526}>Welcome Back!</Text>
         <View style={styles.Input}>
-          <Text style={styles.Txt922}>Email</Text>
+          <Text style={styles.Txt365}>Email</Text>
+        </View>
+        <View style={styles.Input2}>
+          <View style={styles.Input1}>
+            <Text style={styles.Txt365}>Password</Text>
+          </View>
         </View>
         <Text style={styles.multiple1}>Forgot Password? Click Here</Text>
         <View style={styles.Button}>
-          <Text style={styles.Txt971}>Sign In</Text>
+          <Text style={styles.Txt1071}>Sign In</Text>
         </View>
         <Text style={styles.multiple2}>Don’t have an account? Sign Up</Text>
-      </View>
-      <View style={styles.Input2}>
-        <View style={styles.Input1}>
-          <Text style={styles.Txt922}>Password</Text>
-        </View>
       </View>
     </View>
   )
@@ -30,25 +30,23 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "flex-start",
-    position: "relative",
     paddingTop: 178,
-    paddingBottom: 177,
-    paddingLeft: 0,
-    paddingRight: 0,
-    borderRadius: 40,
+    paddingBottom: 161,
+    paddingLeft: 17,
+    paddingRight: 16,
     backgroundColor: "rgba(41,47,63,1)",
     shadowColor: "rgba(24,48,63,0.5)",
     elevation: 10,
     shadowOffset: { width: 40, height: 40 },
-    width: 375,
-    height: 812,
+    width: 411,
+    height: 823,
   },
-  Group852: {
+  Group1053: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   },
-  Txt815: {
+  Txt526: {
     fontSize: 32,
     fontFamily: "Poppins, sans-serif",
     fontWeight: "600",
@@ -56,7 +54,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     justifyContent: "center",
     width: 376,
-    marginBottom: 102,
+    marginBottom: 100,
   },
   Input: {
     display: "flex",
@@ -64,14 +62,43 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     alignItems: "flex-start",
     paddingTop: 10,
-    paddingBottom: 13,
-    paddingLeft: 9,
-    paddingRight: 283,
-    marginBottom: 119,
+    paddingBottom: 13.62,
+    paddingLeft: 10,
+    paddingRight: 313.58,
     borderRadius: 8,
     backgroundColor: "rgba(255, 255, 255, 1)",
   },
-  Txt922: {
+  Txt365: {
+    fontSize: 13,
+    fontFamily: "Poppins, sans-serif",
+    fontWeight: "200",
+    color: "rgba(0,0,0,0.74)",
+  },
+
+  Input2: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    paddingTop: 15,
+    paddingBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    marginBottom: 101,
+  },
+  Input1: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    paddingTop: 10,
+    paddingBottom: 13,
+    paddingLeft: 10,
+    paddingRight: 287,
+    borderRadius: 8,
+    backgroundColor: "rgba(255, 255, 255, 1)",
+  },
+  Txt365: {
     fontSize: 13,
     fontFamily: "Poppins, sans-serif",
     fontWeight: "200",
@@ -79,7 +106,7 @@ const styles = StyleSheet.create({
   },
 
   multiple1: {
-    main: "Txt435",
+    main: "Txt464",
     seg1: "[object Object]",
     seg2: "[object Object]",
     seg3: "[object Object]",
@@ -97,11 +124,11 @@ const styles = StyleSheet.create({
     borderRadius: 13.06,
     backgroundColor: "rgba(0,172,131,1)",
   },
-  Txt971: {
-    fontSize: 13.06,
+  Txt1071: {
+    fontSize: 14,
     fontFamily: "Poppins, sans-serif",
     fontWeight: "700",
-    color: "rgba(255, 255, 255, 0.98)",
+    color: "rgba(255, 255, 255, 1)",
     textAlign: "center",
     justifyContent: "center",
     width: 146,
@@ -109,43 +136,9 @@ const styles = StyleSheet.create({
   },
 
   multiple2: {
-    main: "Txt736",
+    main: "Txt8610",
     seg1: "[object Object]",
     seg2: "[object Object]",
     seg3: "[object Object]",
-  },
-
-  Input2: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    position: "absolute",
-    top: 374,
-    left: 20,
-    paddingTop: 15,
-    paddingBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    width: 329,
-    height: 46,
-  },
-  Input1: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    paddingTop: 10,
-    paddingBottom: 13,
-    paddingLeft: 9,
-    paddingRight: 256,
-    borderRadius: 8,
-    backgroundColor: "rgba(255, 255, 255, 1)",
-  },
-  Txt922: {
-    fontSize: 13,
-    fontFamily: "Poppins, sans-serif",
-    fontWeight: "200",
-    color: "rgba(0,0,0,0.74)",
   },
 })
